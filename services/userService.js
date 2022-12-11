860b7c7055bda071f3be7916d39c82cb16475092
BaeGayeon <ebgy918@gmail.com>
2022-12-11 11:45:05 +0900

    chore: 로그인 변경

--- services/userService.js
+++ services/userService.js
@@ -22,7 +22,7 @@ module.exports = {
         const userResult = await user.findOne({
             where: { phoneNumber: phoneNumber, password: password },
         });
-        if ((userResult == null) | (userResult.dataValues.status == 1)) {
+        if (userResult == null) {
             return null;
         } else {
             session.phoneNumber = userResult.dataValues.phoneNumber;
