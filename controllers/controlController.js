5c253980dc30b2179a91e277afe70c3de8b1e662
BaeGayeon <ebgy918@gmail.com>
2022-12-11 11:42:10 +0900

    chore: opendoor 라우트 수정

--- controllers/controlController.js
+++ controllers/controlController.js
@@ -5,20 +5,6 @@ const request = require('request');
 let url = 'https://angry-olives-cross-211-36-150-124.loca.lt';
 
 module.exports = {
-    openDoor: async function (req, res) {
-        let id = req.params.id;
-        try {
-            let result = await controlService.setOpen(id);
-            if (result) {
-                res.status(200).send(true);
-            } else {
-                res.status(200).send(false);
-            }
-        } catch (err) {
-            console.log(err);
-            res.status(400).send(false);
-        }
-    },
     open: async function (req, res) {
         try {
             console.log('open');
--- routes/controlRoute.js
+++ routes/controlRoute.js
@@ -2,7 +2,7 @@ const express = require('express');
 const controlController = require('../controllers/controlController');
 const router = express.Router();
 
-router.get('/opendoor/:id', controlController.open);
+router.get('/opendoor', controlController.open);
 router.get('/test', controlController.test);
 
 module.exports = router;
