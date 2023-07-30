06142348f434e95f71144d7de9cce5d893dcd750
BaeGayeon <ebgy918@gmail.com>
2023-07-31 05:20:19 +0900

    feat: checkUser middleware 적용

--- routes/deliveryRoute.js
+++ routes/deliveryRoute.js
@@ -2,7 +2,9 @@ const express = require('express');
 const deliveryController = require('../controllers/deliveryController');
 const router = express.Router();
 
-router.post('/order', deliveryController.orderDelivery);
+const checkUser = require('../middleware/checkUser');
+
+router.post('/order', checkUser, deliveryController.orderDelivery);
 router.get('/list/:phoneNumber', deliveryController.getDeliveryList);
 
 module.exports = router;
